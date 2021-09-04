812c8a9abd60a88e958aa38075d629f15431a74a
gastonVR <ivangaston20032017@gmail.com>
2021-09-04 16:46:23 -0300

    cambio de las rutas para las notas

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const {route4GET, route4POST, route4PUT, route4DeleteFisic, route4DeleteLogic} = require('../controllers/notes.controllers');
 
-router.get('/get-notes', route4GET);
-router.post('/post-notes', route4POST);
-router.put('/edit-notes', route4PUT);
+router.get('/notes/all', route4GET);
+router.post('/notes/new', route4POST);
+router.put('/notes/edit', route4PUT);
 //Eliminación lógica
-router.put('/delete-notes', route4DeleteLogic);
+router.put('/notes/delete', route4DeleteLogic);
 //Eliminación física
-router.delete('/delete-notes', route4DeleteFisic);
+router.delete('/notes/delete', route4DeleteFisic);
 
 module.exports = router;
